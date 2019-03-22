bd0f535b1d917390613f75efe3006864897f3bae
Name <name@email>
2019-03-22 15:53:52 +0100

    refactored configure_logging so it will only create a directory, if both, a filename and a directory name are specified.

--- util/log.py
+++ util/log.py
@@ -7,9 +7,13 @@ def configure_logging(fn=None,
                       threshold=logging.DEBUG,
                       fmt='[%(asctime)s] %(name)s - %(levelname)s: %(message)s',
                       date_fmt='%Y/%m/%d %H:%M:%S'):
-    log_file = None
-    if (log_dir is not None) and (fn is not None):
+
+    if fn is not None and log_dir is not None:
         if not os.path.exists(log_dir):
             os.makedirs(log_dir)
-        log_file = os.path.join(log_dir, fn)
-    logging.basicConfig(level=threshold, format=fmt, datefmt=date_fmt, filename=log_file)
+        fn = os.path.join(log_dir, fn)
+    logging.basicConfig(level=threshold, format=fmt, datefmt=date_fmt, filename=fn)
+
+
+if __name__ == '__main__':
+    pass
